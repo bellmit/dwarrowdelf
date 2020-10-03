522e1102677b74300c388a049695cac2d495df54
km3ll <kmell@protonmail.com>
2020-10-03 10:23:47 -0500

    Implement a Kafka embedded server and one subscriber

---
+++ src/main/java/com/bankless/app/kafka/dto/KafkaAccountOpenedEvent.java
@@ -0,0 +1,31 @@
+package com.bankless.app.kafka.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class KafkaAccountOpenedEvent {
+
+	private String dateTime;
+
+	private String countryCode;
+
+	private String no;
+
+	private String balance;
+
+
+	@Override
+	public String toString() {
+		return "KafkaAccountOpenedEvent{" +
+				"dateTime='" + dateTime + '\'' +
+				", countryCode='" + countryCode + '\'' +
+				", no='" + no + '\'' +
+				", balance='" + balance + '\'' +
+				'}';
+	}
+
+}
