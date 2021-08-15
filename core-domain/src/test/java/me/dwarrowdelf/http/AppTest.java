c06dc6caeafc6a037ae03be5f16b866c2ae9e6fb
km3ll <kmell@protonmail.com>
2021-08-15 15:59:55 -0500

    Define application.properties files for modules

--- core-domain/src/test/java/me/dwarrowdelf/http/AppTest.java
+++ core-domain/src/test/java/me/dwarrowdelf/http/AppTest.java
@@ -1,20 +1,47 @@
 package me.dwarrowdelf.http;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.FileInputStream;
+import java.util.Properties;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-	/**
-	 * Rigorous Test :-)
-	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
+
 	@Test
+	@Tag("Unit")
+	@DisplayName("Should answer with true")
 	public void shouldAnswerWithTrue() {
 		assertTrue(true);
 	}
+
+	@Test
+	@Tag("Unit")
+	public void shouldReadPropertiesFile() throws Exception {
+		String root = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+		LOGGER.info("Root path: {}", root);
+
+		String configPath = root + "application.properties";
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(configPath));
+
+		String greeting = properties.getProperty("greeting");
+		assertEquals("hello-motto", greeting);
+	}
+
+	@Test
+	@Tag("Integration")
+	@DisplayName("Should answer with true too")
+	public void shouldAnswerWithTrueToo() {
+		assertTrue(true);
+	}
+
+
 
 }
